ebaa5ec1098a2515860f860cdd0cbc038edefbc9
유의선 <yoogle@hong-gildong-ui-macbook-pro.local>
2018-04-27 14:06:53 +0900

    Initial Commit

---
+++ bank_account/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bank_account
+//
+//  Created by yoogle on 2018. 4. 27..
+//  Copyright © 2018년 yoogle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
